00fdeb915edfaedc993b26e077e125238d77aab0
shAtud <sidalihouda.computerscience@gmail.com>
2022-01-12 14:23:54 +0100

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,36 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { useState } from 'react'
 import styles from '../styles/Home.module.css'
 
+
 export default function Home() {
-  return (
-    <div className='bg-white h-[100vh] w-[100vw] flex justify-center items-center font-thin'>
-      <div className='bg-gradient-to-r from-emerald-300 to-violet-200  rounded-lg shadow-xl shadow-emerald-400 h-[75vh] w-[80vw] animate-fade-left-right sm:h-[550px] sm:w-[600px] md:w-[700px] lg:w-[850px] flex flex-row items-center justify-center'>
-        <div className='text-center animate-fade-in-down sm:basis-1/2 text-3xl hidden sm:flex px-12'>
-        <div className=''>Login form</div>
+  return(
+    <div className='w-[100vw] h-[100vh] flex items-center justify-center'>
+      <Image src ='/2.jpg' layout='fill'/>
+      <div className='bg-white/20 backdrop-blur-xl w-[75vw] h-[75vh] sm:w-[500px] sm:h-[550px] md:w-[650px] lg:w-[800px] rounded-xl font-mono font-thin'>
+        <div className='text-4xl pt-8 pb-16 tracking-wider text-center'>Login Form</div>
+        <div className='md:px-32 lg:px-48 px-8'>
+        <table>
+          <tr>
+            <td className='text-2xl py-4'>User Name</td>
+            <td><input className='text-2xl bg-transparent w-[130px] placeholder-black/50 text-black/75' placeholder='Name...'/></td>
+          </tr>
+          <tr>
+            <td className='text-2xl py-4'>Email</td>
+            <td><input className='text-2xl bg-transparent w-[130px] placeholder-black/50 text-black/75' placeholder='Email...'/></td>
+          </tr>
+          <tr>
+            <td className='text-2xl py-4 pr-32'>Password</td>
+            <td><input type='password' className='text-2xl bg-transparent w-[130px] placeholder-black/50 ' placeholder='Password...'/></td>
+          </tr>
+          <tr>
+            <td className='text-2xl py-4 pb-16'>Confirm Password</td>
+            <td><input type='password' className='text-2xl bg-transparent w-[130px] pb-12 placeholder-black/50 ' placeholder='Password...'/></td>
+          </tr>
+        </table>
         </div>
-        <div className='basis-11/12'>
-          <div className='sm:text-2xl text-center sm:font-thin font-semibold text-3xl sm:text-left pb-8'>Login</div>
-          <table className='text-xl w-[80vw] sm:w-auto'>
-            <tr className=''>
-              <td className='py-4 pr-8 pl-12 sm:pl-0'>Full Name:</td>
-              <td>
-                <input className='bg-transparent' placeholder='Name...'></input>
-              </td>
-            </tr>
-            <tr>
-              <td className='py-4 pr-24 pl-12 sm:pl-0'>Email:</td>
-              <td>
-                <input className='bg-transparent' placeholder='Email...' type='email'></input>
-              </td>
-            </tr>
-            <tr>
-              <td className='py-4 pl-12 sm:pl-0'>Create Password:</td>
-              <td>
-                <input className='bg-transparent' placeholder='Password...' type='password'></input>
-              </td>
-            </tr>
-            <tr className=''>
-              <td className='py-4 pr-16 pb-12 pl-12 sm:pl-0'>Confirm password:</td>
-              <td>
-                <input className='bg-transparent pr-16 pb-12' placeholder='Password...' type='password'></input>
-              </td>
-            </tr>
-          </table>
-          <div className='text-center sm:text-left'>
-          <button className='m-auto bg-gradient-to-r from-emerald-400 to-violet-300 hover:from-emerald-500 hover:to-violet-400 shadow-violet-300 shadow-md rounded-full h-[35px] w-[120px] text-center text-xl font-thin'>Login</button>
-          </div>
-        </div>
+        <button className='text-2xl h-[40px] w-[130px] bg-white/10 rounded-full hover:bg-white/20 sm:mx-48 mx-32 md:mx-80'>Login</button>
       </div>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,49 +3,9 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
+  
   theme: {
-    extend: {},
   },
   plugins: [],
-  theme: {
-    extend: {
-        keyframes: {
-            'fade-left-right': {
-                '0%': {
-                    opacity: '0',
-                    transform: 'translateX(-30px)'
-                },
-                '100%': {
-                    opacity: '1',
-                    transform: 'translateX(0px)',
-
-                },
-            },
-            'fade-in-down': {
-              '0%': {
-                  opacity: '0',
-                  transform: 'translateY(150px)'
-              },
-              '25%': {
-                  opacity: '1',
-                  transform: 'translateY(75px)'
-              },
-              '50%': {
-                opacity: '1',
-                transform: 'translateY(25px)'
-            },
-            '100%': {
-              opacity: '1',
-              transform: 'translateY(0)'
-          },
-          },
-        },
-        animation: {
-            'fade-left-right': 'fade-left-right 2s ease-out',
-            'fade-in-down': 'fade-in-down 1s ease-out',
-        }
-    },
-},
-variants: {},
-plugins: [],
+  variants: {},
 }
